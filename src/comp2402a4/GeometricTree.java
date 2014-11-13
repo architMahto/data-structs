b2d6ab78ce9d484c6179fae80f9dda5037f07cde
Archit Mahto <architmahto@cmail.carleton.ca>
2014-11-13 16:14:23 -0500

    Modified GeometricTree.java

--- src/comp2402a4/GeometricTree.java
+++ src/comp2402a4/GeometricTree.java
@@ -1,5 +1,7 @@
 package comp2402a4;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 
 public class GeometricTree extends BinaryTree<GeometricTreeNode> {
@@ -11,10 +13,38 @@ public class GeometricTree extends BinaryTree<GeometricTreeNode> {
 	public void inorderDraw() {
 		assignLevels();
 		// TODO: use your code here instead
-		Random rand = new Random();
-		randomX(r, rand);
+		//Random rand = new Random();
+		//randomX(r, rand);
+		GeometricTreeNode u = r, prev = nil, next;
+		int xCounter = 0;
+		while (u != nil) {
+			if (prev == u.parent) {
+				if (u.left != nil) {next = u.left;}
+				else if (u.right != nil) {next = u.right;}
+				else {
+					u.position.x = xCounter;
+					xCounter++;
+					next = u.parent;
+				}
+			} else if (prev == u.left) {
+				u.position.x = xCounter;
+				xCounter++;
+				if (u.right != nil) {
+					next = u.right;
+				} else {
+					next = u.parent;
+				}
+			} else {
+				next = u.parent;
+			}
+			prev = u;
+			u = next;
+		}
 	}
 	
+	protected void assignX(GeometricTreeNode u) {
+		if (u == null) return;
+	}
 	
 	protected void randomX(GeometricTreeNode u, Random r) {
 		if (u == null) return;
@@ -31,8 +61,25 @@ public class GeometricTree extends BinaryTree<GeometricTreeNode> {
 	 */
 	public void leftistDraw() {
 		assignLevels();
-		Random rand = new Random();
-		randomX(r, rand);
+		Queue<GeometricTreeNode> q = new LinkedList<GeometricTreeNode>();
+		q.add(r);
+		
+		GeometricTreeNode prev = null;
+		int xPos = 0;
+		
+		while (!q.isEmpty()) {
+			GeometricTreeNode u = q.remove();
+			
+			if (u == r) xPos = 0;
+			else if (u.position.y > prev.position.y) xPos = 0;
+			else xPos++;
+			
+			if (u.left != nil) q.add(u.left);
+			if (u.right != nil) q.add(u.right);
+			
+			u.position.x = xPos;
+			prev = u;
+		}
 	}
 	
 	
