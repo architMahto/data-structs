2bd56ba01968520f4aebdad0bc4639673b01271b
Archit Mahto <architmahto@cmail.carleton.ca>
2014-11-27 15:58:24 -0500

    Commiting comp2402a5 package

---
+++ src/comp2402a5/AVLTreeTester.java
@@ -0,0 +1,10 @@
+package comp2402a5;
+
+public class AVLTreeTester {
+    public static boolean testAVLTree(AVLTree<Integer> t) {
+        // Add your test code here.
+        // This code should return true if t is a properly implemented AVLTree.
+        // Otherwise, it should return false, crash, or take too long to execute        
+        return false;
+    }
+}
